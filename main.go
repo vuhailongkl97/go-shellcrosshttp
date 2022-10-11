96db9f649dc713fc492e76efdd3c4b1d72c71752
long.vu <vuhailongkl97@gmail.com>
2022-10-11 19:07:30 +0700

    support cmd via GET and show code prettily

--- main.go
+++ main.go
@@ -10,6 +10,16 @@ import (
 	"strings"
 )
 
+func sendForm(w http.ResponseWriter) {
+
+	w.Header().Set("Content-type", "text/html")
+	str := `<form action="/" method="GET" target="">
+		     <label for="command">command:</label>
+		       <input type="text" id="command" name="command" size=100 autofocus><br><br>
+			   </form>`
+	w.Write([]byte(str))
+}
+
 func doCommand(w http.ResponseWriter, cmd string, arg ...string) error {
 
 	switch cmd {
@@ -18,40 +28,41 @@ func doCommand(w http.ResponseWriter, cmd string, arg ...string) error {
 	default:
 		break
 	}
+
 	l_cmd := exec.Command(cmd, arg...)
 	fmt.Printf("doing %v args %v \n", cmd, arg)
 	stdout, err := l_cmd.StdoutPipe()
 	if err != nil {
-		log.Fatal(err)
 		return err
 	}
 
 	if err := l_cmd.Start(); err != nil {
-		log.Fatal(err)
 		return err
 	}
 
 	rdr := bufio.NewReader(stdout)
-	w.Header().Set("Content-type", "text/html")
+	sendForm(w)
+	w.Write([]byte(`<textarea disabled="true" style="border: none;background-color:white;width:100%;height:100%;">`))
 	for {
 		line, _, err := rdr.ReadLine()
 		if err != nil {
 			break
 		}
 		w.Write(line)
-		w.Write([]byte("<br>"))
+		w.Write([]byte("\n"))
 		fmt.Println(string(line))
 	}
+	w.Write([]byte("</textarea>"))
 
 	l_cmd.Wait()
 	return nil
 
 }
 
-func handler(w http.ResponseWriter, req *http.Request) {
+func handleGET(w http.ResponseWriter, req *http.Request) {
 	cmd := req.URL.RequestURI()
-	// ignore / character
-	cmd = cmd[1:]
+	cmd = strings.TrimPrefix(cmd, "/")
+	cmd = strings.TrimPrefix(cmd, "?command=")
 
 	decodedValue, err := url.QueryUnescape(cmd)
 	if err != nil {
@@ -60,17 +71,36 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	lcmd := strings.Fields(decodedValue)
-	err = doCommand(w, lcmd[0], lcmd[1:]...)
+	if err != nil || len(lcmd) == 0 {
+		sendForm(w)
+		return
+	}
+
+	if len(lcmd) > 1 {
+		err = doCommand(w, lcmd[0], lcmd[1:]...)
+	} else {
+		err = doCommand(w, lcmd[0])
+	}
 
 	if err != nil {
-		log.Fatal(err)
+		sendForm(w)
+		return
+	}
+
+}
+func handler(w http.ResponseWriter, req *http.Request) {
+
+	switch req.Method {
+	case http.MethodGet:
+		handleGET(w, req)
+	default:
+		fmt.Printf("default case %v\n", req.Method)
 	}
 
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-
 	if err := http.ListenAndServe(":1997", nil); err != nil {
 		log.Fatal(err)
 	}
