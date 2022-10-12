f7886a7d19cd12d89e481f51714be03844dcaeba
long.vu <vuhailongkl97@gmail.com>
2022-10-12 16:37:32 +0700

    add flag , POST file support

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	"log"
+	"flag"
+	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 	"os/exec"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sendForm(w http.ResponseWriter) {
@@ -16,7 +19,15 @@ func sendForm(w http.ResponseWriter) {
 	str := `<form action="/" method="GET" target="">
 		     <label for="command">command:</label>
 		       <input type="text" id="command" name="command" size=100 autofocus><br><br>
-			   </form>`
+			   </form>
+<form
+      enctype="multipart/form-data"
+      action="/upload"
+      method="post"
+    >
+      <input type="file" name="myFile" />
+      <input type="submit" value="upload" />
+    </form>`
 	w.Write([]byte(str))
 }
 
@@ -30,7 +41,7 @@ func doCommand(w http.ResponseWriter, cmd string, arg ...string) error {
 	}
 
 	l_cmd := exec.Command(cmd, arg...)
-	fmt.Printf("doing %v args %v \n", cmd, arg)
+	//fmt.Printf("doing %v args %v \n", cmd, arg)
 	stdout, err := l_cmd.StdoutPipe()
 	if err != nil {
 		return err
@@ -52,7 +63,9 @@ func doCommand(w http.ResponseWriter, cmd string, arg ...string) error {
 		str_line = strings.ReplaceAll(str_line, "textarea", "kextarea")
 		w.Write([]byte(str_line))
 		w.Write([]byte("\n"))
-		fmt.Println(string(line))
+		if *debug {
+			//fmt.Println(string(line))
+		}
 	}
 	w.Write([]byte("</textarea>"))
 
@@ -68,7 +81,7 @@ func handleGET(w http.ResponseWriter, req *http.Request) {
 
 	decodedValue, err := url.QueryUnescape(cmd)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
 		return
 	}
 
@@ -96,14 +109,82 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	case http.MethodGet:
 		handleGET(w, req)
 	default:
-		fmt.Printf("default case %v\n", req.Method)
+		if *debug {
+			//fmt.Printf("default case %v\n", req.Method)
+		}
 	}
 
 }
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	if *debug {
+		//fmt.Println("File Upload Endpoint Hit")
+	}
+
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	r.ParseMultipartForm(10 << 20)
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+
+		if *debug {
+			//fmt.Println("Error Retrieving the File")
+			//fmt.Println(err)
+		}
+		return
+	}
+	defer file.Close()
+
+	if *debug {
+		//fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+		//fmt.Printf("File Size: %+v\n", handler.Size)
+		//fmt.Printf("MIME Header: %+v\n", handler.Header)
+	}
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+
+	if _, err := os.Stat("uploads"); os.IsNotExist(err) {
+		err = os.Mkdir("uploads", os.ModePerm)
+		if err != nil {
+
+			if *debug {
+				//fmt.Printf("error happend %v\n", err)
+			}
+			return
+		}
+	}
+
+	tempFile, err := ioutil.TempFile("uploads", strconv.Itoa(time.Now().Minute())+"-*-"+handler.Filename)
+	if err != nil {
+		//fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		//fmt.Println(err)
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+	// return that we have successfully uploaded our file!
+	sendForm(w)
+	//fmt.Fprintf(w, "Success uploaded file\n")
+
+}
+
+var debug *bool
 
 func main() {
+
+	debug = flag.Bool("debug", false, "debug flag")
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/upload", uploadFile)
 	if err := http.ListenAndServe(":1997", nil); err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
 	}
 }
